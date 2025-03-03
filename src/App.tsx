7662b5b5e68bc0377352e43167c74c277150c727
Leonardo Ferreira <leonardo.rocha0803@gmail.com>
2025-03-03 18:37:54 -0300

    add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ShiftDetails from "./pages/ShiftDetails";
+import NotFound from "./pages/NotFound";
 
 const App = () => (
   <>
@@ -11,6 +12,7 @@ const App = () => (
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/shift/:id" element={<ShiftDetails />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </>
